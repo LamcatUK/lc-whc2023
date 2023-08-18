72cdfb16fad985fe694fef4a10c25dbd2592dfa8
CheechRockwizard <github@lamcat.co.uk>
2023-08-18 16:43:15 +0100

    added FAQ block

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -65,6 +65,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'lc_faq',
+            'title'				=> __('LC FAQ'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/lc_faq.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
---
+++ page-templates/blocks/lc_faq.php
@@ -0,0 +1,44 @@
+<section class="faqs py-5">
+    <div class="container-xl">
+        <h2 class="mb-4"><?=get_field('title')?>
+        </h2>
+        <?php
+    $accordion = random_str(5);
+
+        echo '<div itemscope="" itemtype="https://schema.org/FAQPage" id="accordion' .  $accordion . '" class="accordion accordion-flush">';
+        $counter = 0;
+        $show = 'show';
+        $collapsed = '';
+        while (have_rows('faqs')) {
+            the_row();
+            $ac = $accordion . '_' . $counter;
+            ?>
+        <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
+            <div class="accordion-head accordion-collapse <?=$collapsed?>"
+                itemprop="name" data-bs-toggle="collapse"
+                id="heading_<?=$ac?>"
+                data-bs-target="#c<?=$ac?>" aria-expanded="true"
+                aria-controls="collapse_<?=$ac?>">
+                <div class="pb-1">
+                    <?=get_sub_field('question')?>
+                </div>
+            </div>
+            <div class="collapse <?=$show?>" itemscope=""
+                itemprop="acceptedAnswer" itemtype="https://schema.org/Answer"
+                id="c<?=$ac?>"
+                aria-labelledby="heading_<?=$ac?>"
+                data-bs-parent="#accordion<?=$accordion?>">
+                <div itemprop="text" class="faq__answer">
+                    <?=get_sub_field('answer')?>
+                </div>
+            </div>
+        </div>
+        <?php
+            $counter++;
+            $show = '';
+            $collapsed = 'collapsed';
+        }
+        echo '</div>';
+        ?>
+    </div>
+</section>
