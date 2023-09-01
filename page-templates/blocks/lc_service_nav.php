d7d2746a8d0ab58ea581b9d0c0b18cca5655ebb1
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 17:17:05 +0100

    Update lc_service_nav.php

--- page-templates/blocks/lc_service_nav.php
+++ page-templates/blocks/lc_service_nav.php
@@ -2,7 +2,8 @@
     <div class="container-xl">
         <div class="service_nav__content">
             <h2 class="has-primary-400-color">
-                <?=get_field('title')?></h2>
+                <?=get_field('title')?>
+            </h2>
             <div><?=get_field('content')?></div>
         </div>
         <div class=" service_nav__grid">
